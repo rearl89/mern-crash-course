beee097290b28f480f526c82a453b8c1935fa7eb
Roger Earl <rogerearl@me.com>
2024-11-01 22:38:12 -0700

    Added update method.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -36,6 +37,27 @@ app.post("/api/products", async (req, res) => {
   } catch (error) {
     console.error("Error in Create product:", error.message);
     res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.patch("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid product id" });
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ success: true, product: updatedProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Product not found" });
   }
 });
 
