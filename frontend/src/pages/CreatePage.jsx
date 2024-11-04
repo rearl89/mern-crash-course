bf8eda6bcdb8c8b7f5343277fbe68f1199dcdee7
Roger Earl <rogerearl@me.com>
2024-11-03 19:56:47 -0800

    Made home page when no products are available.

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -5,6 +5,7 @@ import {
   Heading,
   Input,
   useColorModeValue,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
 import { useState } from "react";
@@ -16,13 +17,32 @@ const CreatePage = () => {
     price: "",
     image: "",
   });
+  const toast = useToast();
 
   const { createProduct } = useProductStore();
 
   const handleSubmit = async () => {
     const { success, message } = await createProduct(newProduct);
-    console.log("Success:", success);
-    console.log("Message:", message);
+    if (!success) {
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: "Success",
+        description: message,
+        status: "success",
+        isClosable: true,
+      });
+      setNewProduct({
+        name: "",
+        price: "",
+        image: "",
+      });
+    }
   };
 
   return (
